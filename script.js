d5e7fb5fcd4b52cfe1bd1b3d184a9294177a4eca
Grimm-Meeper <fenton@vonmillion.family>
2026-09-09 17:31:59 +0000

    parts 1 - 3

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //starter code
 
 function run() {
-    sleep("Tigger");
+    var tigger = new Tiger("Tigger");
+    tigger.eat("meat");
+    tigger.eat("kibble");
   }
-  
-  function sleep(name) {
-    console.log(name + " sleeps for 8 hours")
-  }
+
+class Tiger {
+
+    constructor(name) {
+        this.name = name
+        this.favoriteFood = "meat"
+    }
+
+    sleep() {
+      console.log(this.name + " sleeps for 8 hours");
+    }
+
+    eat(food) {
+      console.log(this.name + " eats " + food)
+      if (food == this.favoriteFood) {
+        console.log("YUM!!! " + this.name + " wants more " + food)
+      } else {
+        this.sleep(this.name)
+      }
+    }
+}
+
